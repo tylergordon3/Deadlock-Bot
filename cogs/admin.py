4b9c9917e650434950ba1d4578618544b137370a
tylergordon3 <tmgordon33@gmail.com>
2025-03-09 20:28:11 -0400

    3.9 switch back

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,12 +1,15 @@
-from discord.ext import commands
+from discord.ext import commands, tasks
 import os
 import tools.reqData as rd
 import datetime as dt
+
+guild_id = 546868043838390299
 
 
 class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.odysseyCountdown.start()
 
     @commands.command()
     async def reload(self, ctx):
@@ -23,6 +26,30 @@ class Admin(commands.Cog):
     @commands.command()
     async def shutdown(self, ctx):
         await ctx.bot.logout()
+
+    @commands.command()
+    async def name(self, ctx):
+        steve = 549708233640509442
+        guild = await self.bot.fetch_guild(guild_id)
+        steve_ob = await guild.fetch_member(steve)
+
+        release = "2026/07/17"
+        releaseDt = dt.datetime.strptime(release, "%Y/%m/%d")
+        now = dt.datetime.now()
+        diff = (releaseDt - now).days
+        await steve_ob.edit(nick=str(diff))
+
+    @tasks.loop(hours=12)
+    async def odysseyCountdown(self):
+        steve = 549708233640509442
+        guild = await self.bot.fetch_guild(guild_id)
+        steve_ob = await guild.fetch_member(steve)
+
+        release = "2026/07/17"
+        releaseDt = dt.datetime.strptime(release, "%Y/%m/%d")
+        now = dt.datetime.now()
+        diff = (releaseDt - now).days
+        await steve_ob.edit(nick=str(diff))
 
 
 async def setup(bot):
