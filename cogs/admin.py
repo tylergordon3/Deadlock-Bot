722aa11eb4e5f6278926945b15edba7fefb1a8e6
tylergordon3 <tmgordon33@gmail.com>
2025-12-18 18:45:59 -0500

    adding loop for website

--- cogs/admin.py
+++ cogs/admin.py
@@ -2,12 +2,14 @@ from discord.ext import commands, tasks
 import os
 import tools.reqData as rd
 import datetime as dt
+import subprocess
 
 guild_id = os.environ.get("GUILD_ID")
 
 class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.hourly_deploy.start()
 
     @commands.command()
     async def reload(self, ctx):
@@ -24,6 +26,19 @@ class Admin(commands.Cog):
     @commands.command()
     async def shutdown(self, ctx):
         await ctx.bot.logout()
+
+    @tasks.loop(hours=1)
+    async def hourly_deploy(self):
+        curr_datetime = dt.datetime.now()
+        print(f"Attempting to deploy CBB Website. Time: {curr_datetime}")
+        try:
+            subprocess.run(
+                ["python", r"C:\Users\tmgor\OneDrive\Desktop\deploy.bat"],
+                check=True
+            )
+            print("Script ran successfully")
+        except Exception as e:
+            print(f"Script failed: {e}")
 
 async def setup(bot):
     await bot.add_cog(Admin(bot))
